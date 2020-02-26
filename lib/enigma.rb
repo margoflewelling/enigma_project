dca0a3cb7d3ab1cc928f9e5ef54bb0dc64089a12
Margo Flewelling <flewelling.margo@gmail.com>
2020-02-26 13:20:45 -0700

    Add functionality for encryption

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,69 @@
 class Enigma
 
+  def initialize
+  end
+
+  attr_accessor :key, :date
+
+  def encrypt_message(message, key, date)
+    message_letters = split_message(message)
+    encrypted = ""
+    loop do
+      index = alphabet.index(message_letters[0])
+      encrypted << alphabet.rotate(final_shift(key, date)["A"])[index]
+      break if encrypted.length == message_letters.length
+      index = alphabet.index(message_letters[1])
+      encrypted << alphabet.rotate(final_shift(key, date)["B"])[index]
+      break if encrypted.length == message_letters.length
+      index = alphabet.index(message_letters[2])
+      encrypted << alphabet.rotate(final_shift(key, date)["C"])[index]
+      break if encrypted.length == message_letters.length
+      index = alphabet.index(message_letters[3])
+      encrypted << alphabet.rotate(final_shift(key, date)["D"])[index]
+      break if encrypted.length == message_letters.length
+      message_letters = message_letters.rotate(4)
+     end
+     encrypted
+  end
+
+  def split_message(message)
+    clean_message = ""
+    message.each_char do |char|
+      clean_message << char if alphabet.include?(char)
+     end
+    clean_message.split("")
+  end
+
+  def alphabet
+    ("a".."z").to_a << " "
+  end
+
+  def create_keys(key)
+    keys = {}
+    keys["A"] = key[0..1]
+    keys["B"] = key[1..2]
+    keys["C"] = key[2..3]
+    keys["D"] = key[3..4]
+    keys
+  end
+
+  def create_offsets(date)
+    date_squared = (date.to_i * date.to_i).to_s
+    last_four = date_squared[-4..-1]
+    offsets = {}
+    offsets["A"] = last_four[0]
+    offsets["B"] = last_four[1]
+    offsets["C"] = last_four[2]
+    offsets["D"] = last_four[3]
+    offsets
+  end
+
+  def final_shift(key, date)
+    final_shift = {}
+    create_keys(key).map do |num_key, key|
+      final_shift[num_key] = key.to_i + create_offsets(date)[num_key].to_i
+    end
+    final_shift
+  end
+
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,4 +3,50 @@ require './lib/enigma'
 require 'mocha/minitest'
 
 class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+
+  def test_it_can_create_alphabet
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+                "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+                "w", "x", "y", "z", " "
+                ]
+    assert_equal expected, @enigma.alphabet
+  end
+
+  def test_it_can_create_the_keys
+    expected = {"A"=>"02", "B"=>"27", "C"=>"71", "D"=>"15"}
+    assert_equal expected, @enigma.create_keys("02715")
+  end
+
+  def test_it_can_create_the_offset
+    expected = {"A"=>"1", "B"=>"0", "C"=>"2", "D"=>"5"}
+    assert_equal expected, @enigma.create_offsets("040895")
+  end
+
+  def test_creating_final_shift
+    expected = {"A"=>3, "B"=>27, "C"=>73, "D"=>20}
+    assert_equal expected, @enigma.final_shift("02715", "040895")
+  end
+
+  def test_it_can_split_message_by_character
+    expected = ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
+    assert_equal expected, @enigma.split_message("hello world")
+    assert_equal ["h", "i"], @enigma.split_message("h,*#i")
+  end
+
+  def test_it_can_encrypt_message
+    assert_equal "keder ohulw", @enigma.encrypt_message("hello world", "02715", "040895")
+    assert_equal "pysgdmxtlssfdrzh", @enigma.encrypt_message("my name is margo", "02715", "040895")
+  end
+
+
+
 end
