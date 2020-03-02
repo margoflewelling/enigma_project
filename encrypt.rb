ebdc6fbaedeb7d844919a0de434c233df259d739
Margo Flewelling <flewelling.margo@gmail.com>
2020-03-02 14:55:31 -0700

    Fix encrypt file to be command line interface

--- encrypt.rb
+++ encrypt.rb
@@ -1,15 +1,12 @@
 require './lib/enigma'
 enigma = Enigma.new
 
-puts "Where is the file located that you want to encrypt?"
-file_with_message_to_encrypt = gets.chomp
+file_with_message_to_encrypt = ARGV[0]
 message = File.read(file_with_message_to_encrypt)
-puts "What file do you want to write your encrypted message in?"
-file_to_write_to = gets.chomp
+file_to_write_to = ARGV[1]
 encrypted_message = File.open(file_to_write_to, "w")
 encryption = enigma.encrypt(message)
 encrypted_message.write(encryption[:encryption])
-require "pry"; binding.pry
 encrypted_message.close
 
 puts "Created #{file_to_write_to} with the key #{(encryption[:key])} and date #{encryption[:date]}"
