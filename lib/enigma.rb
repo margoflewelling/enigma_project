c2b0d770710a808ed8d0ad3942fb1f8c5a442422
Margo Flewelling <flewelling.margo@gmail.com>
2020-03-02 09:35:14 -0700

    Create alphabet and shifts

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,4 +4,37 @@ class Enigma
   def initialize
   end
 
+  def alphabet
+    ("a".."z").to_a << " "
+  end
+
+  def create_keys(key)
+    keys = {}
+    keys["A"] = key[0..1]
+    keys["B"] = key[1..2]
+    keys["C"] = key[2..3]
+    keys["D"] = key[3..4]
+    keys
+  end
+
+  def create_offsets(date)
+    date_squared = (date.to_i * date.to_i).to_s
+    last_four = date_squared[-4..-1]
+    offsets = {}
+    offsets["A"] = last_four[0]
+    offsets["B"] = last_four[1]
+    offsets["C"] = last_four[2]
+    offsets["D"] = last_four[3]
+    offsets
+  end
+
+  def final_shift(key, date)
+    final_shift = {}
+    create_keys(key).map do |num_key, key|
+      final_shift[num_key] = key.to_i + create_offsets(date)[num_key].to_i
+    end
+    final_shift
+  end
+
+
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,4 +12,27 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_it_can_create_alphabet
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+                "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+                "w", "x", "y", "z", " "
+                ]
+    assert_equal expected, @enigma.alphabet
+  end
+
+  def test_it_can_create_the_keys
+    expected = {"A"=>"02", "B"=>"27", "C"=>"71", "D"=>"15"}
+    assert_equal expected, @enigma.create_keys("02715")
+  end
+
+  def test_it_can_create_the_offset
+    expected = {"A"=>"1", "B"=>"0", "C"=>"2", "D"=>"5"}
+    assert_equal expected, @enigma.create_offsets("040895")
+  end
+
+  def test_creating_final_shift
+    expected = {"A"=>3, "B"=>27, "C"=>73, "D"=>20}
+    assert_equal expected, @enigma.final_shift("02715", "040895")
+  end
+
 end
