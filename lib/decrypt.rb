9b0d6fe5e91f6e6ae938419b02c195eb5ba8405e
Margo Flewelling <flewelling.margo@gmail.com>
2020-03-02 16:49:33 -0700

    Clean up decrypt file

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,10 +1,8 @@
 require './lib/enigma'
 enigma = Enigma.new
 
-file_with_message_to_decrypt = ARGV[0]
-message = File.read(file_with_message_to_decrypt)
-file_to_write_to = ARGV[1]
-decrypted_message = File.open(file_to_write_to, "w")
+message = File.read(ARGV[0])
+decrypted_message = File.open(ARGV[1], "w")
 key = ARGV[2]
 date = ARGV[3]
 
@@ -12,4 +10,4 @@ decryption = enigma.decrypt(message, key, date)
 decrypted_message.write(decryption[:decryption])
 decrypted_message.close
 
-puts "Created #{file_to_write_to} with the key #{key} and date #{date}"
+puts "Created #{ARGV[1]} with the key #{key} and date #{date}"
