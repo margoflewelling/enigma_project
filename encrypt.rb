5dca9d308fcb0e2f4d4ebf1c013dfbd1bb81e78a
Margo Flewelling <flewelling.margo@gmail.com>
2020-03-02 10:11:06 -0700

    Add encryption runner file

---
+++ encrypt.rb
@@ -0,0 +1,13 @@
+require './lib/enigma'
+enigma = Enigma.new
+
+puts "Where is the file located that you want to encrypt?"
+file_with_message_to_encrypt = gets.chomp
+message = File.read(file_with_message_to_encrypt)
+puts "Where is the file you want to write your encrypted message in?"
+file_to_write_to = gets.chomp
+encrypted_message = File.open(file_to_write_to, "w")
+encrypted_message.write((enigma.encrypt(message))[:encryption])
+encrypted_message.close
+
+puts "Created #{file_to_write_to} with the key #{(enigma.encrypt(message))[:key]} and date #{(enigma.encrypt(message))[:date]}"
